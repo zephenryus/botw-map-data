b6acdb86d5b26253705aee844d7c3ebf9b82262b
Kevin Jensen <kevin@turtlerockweb.com>
2018-09-05 23:32:46 -0600

    updated marker type extractor

--- csv-to-sql.py
+++ csv-to-sql.py
@@ -3,11 +3,13 @@ import math
 import subprocess
 from datetime import datetime
 import pymysql.cursors
+from pymysql import IntegrityError
+
 
 class ConvertCSVtoSQL:
     connection = None
 
-    def __init__(self, path: str, refresh_db=False):
+    def __init__(self, path: str, refresh_db=False, skip_to=0):
         self.start_time = datetime.now()
         self.on_start()
 
@@ -16,7 +18,7 @@ class ConvertCSVtoSQL:
 
         self.database_connect()
 
-        self.convert_file(path)
+        self.convert_file(path, skip_to)
 
     def on_start(self):
         print("Scan started at {0}".format(self.start_time.strftime("%Y-%m-%d %H:%M:%S.%f")))
@@ -27,7 +29,7 @@ class ConvertCSVtoSQL:
         delta = end_time - self.start_time
         print("Scan took {0} seconds".format(delta.total_seconds()))
 
-    def convert_file(self, path):
+    def convert_file(self, path, skip_to=0):
         print('Converting {0}...'.format(path))
         lines_read = 0
 
@@ -38,6 +40,9 @@ class ConvertCSVtoSQL:
                 if lines_read % 100 == 0:
                     print("{0} lines read...".format(lines_read))
 
+                if lines_read < skip_to:
+                    continue
+
                 current_line = line.split(',')
 
                 for index in range(len(current_line)):
@@ -47,15 +52,33 @@ class ConvertCSVtoSQL:
                 map_region_z = math.floor(float(current_line[3]) / 1000) + 4
                 map_region = (map_region_z * 10 + map_region_x) + 1
 
-                self.query("INSERT INTO `markers` (`id`, `map_region_id`, `marker_name`, `x`, `y`, `z`, `source`, `marker_type_id`, `marker_category_id`, `marker_sub_category_id`, `created_at`, `updated_at`) VALUES (NULL, '{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', NULL, NULL);".format(map_region, current_line[0], float(current_line[1]), float(current_line[2]), float(current_line[3]), path, 1, 1, 1))
+                # Objects placed outside of the playable area
+                if map_region < 0:
+                    map_region = 0
+                if map_region > 80:
+                    map_region = 0
+
+                name = self.query("SELECT * FROM `marker_names` WHERE `marker_names`.`marker_name` = '{0}'".format(current_line[0]))
+
+                if len(name) == 0:
+                    self.query("INSERT INTO `marker_names` (`marker_names`.`id`, `marker_names`.`marker_name`) VALUES (NULL, '{0}');".format(current_line[0]))
+                    name = self.query("SELECT * FROM `marker_names` WHERE `marker_names`.`marker_name` = '{0}'".format(
+                        current_line[0]))
+
+                name_id = name[0]['id']
+
+                self.query("INSERT INTO `markers` (`id`, `map_region_id`, `marker_name_id`, `x`, `y`, `z`, `source`, `marker_type_id`, `marker_category_id`, `marker_sub_category_id`, `created_at`, `updated_at`) VALUES (NULL, '{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', NULL, NULL);".format(map_region, name_id, float(current_line[1]), float(current_line[2]), float(current_line[3]), path, 1, 1, 1))
 
     def query(self, query: str, data=()) -> tuple:
         result = []
 
-        # try:
-        with self.connection.cursor() as cursor:
-            cursor.execute(query, data)
-            result = cursor.fetchall()
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(query, data)
+                result = cursor.fetchall()
+        except IntegrityError as e:
+            print('Got error {!r}, errno is {}'.format(e, e.args[0]))
+
         # finally:
         #     self.connection.close()
 
@@ -90,7 +113,7 @@ class ConvertCSVtoSQL:
 
 
 def main():
-    ConvertCSVtoSQL("output/dlc.csv", True)
+    ConvertCSVtoSQL("output/dlc.csv", False)
 
 
 if __name__ == "__main__":
